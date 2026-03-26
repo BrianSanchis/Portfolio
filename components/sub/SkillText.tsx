c9cc1d6f571463b89eb01dd1693955534f0879aa
Brian_Sanchis <b.sanchis@eleve.leschartreux.net>
2026-03-26 12:07:19 +0100

    fix

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -20,7 +20,7 @@ const SkillText = () => {
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
         >
-            Les outils que j'ai utilisé
+            Les outils que j&apos;ai utilisé
         </motion.div>
         <motion.div
         variants={slideInFromRight(0.5)}
